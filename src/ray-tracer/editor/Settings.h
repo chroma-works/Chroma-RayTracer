4810e51dd6380a808456bdf04e8304a65da0c18b
Alper Ş <alpersahistan@gmail.com>
2020-02-22 01:29:24 +0300

    Implement Settings struct

---
+++ src/ray-tracer/editor/Settings.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <string>
+#include <thirdparty/glm/glm/glm.hpp>
+
+namespace Chroma
+{
+	struct Settings
+	{
+		int thread_count = 8;
+		std::string act_editor_cam_name = "";
+		std::string act_rt_cam_name = "";
+		glm::ivec2 resolution = {800, 800};
+		float camera_move_speed = 0.4;
+		float camera_rotate_speed = 0.005;
+		float shadow_eps = 0.001;
+		float intersection_eps = 0.000001;
+		bool calc_shadows = true;
+	};
+}
