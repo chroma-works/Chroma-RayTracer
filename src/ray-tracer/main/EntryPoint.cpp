5939d48c18f2540a954c7651c8815caf1a37ec00
Alper Ş <alpersahistan@gmail.com>
2020-02-29 03:35:38 +0300

    Synch with local

--- src/ray-tracer/main/EntryPoint.cpp
+++ src/ray-tracer/main/EntryPoint.cpp
@@ -28,7 +28,7 @@ int main()
 	Chroma::Shader* shader = Chroma::Shader::ReadAndBuildShaderFromFile("../../assets/shaders/phong.vert", "../../assets/shaders/phong.frag");
 
 	std::shared_ptr<Chroma::Scene> scene;
-	scene = std::make_shared<Chroma::Scene>(*(Chroma::AssetImporter::LoadSceneFromXML(shader, "../../assets/scenes/hw1/cornellbox.xml")));
+	scene = std::make_shared<Chroma::Scene>(*(Chroma::AssetImporter::LoadSceneFromXML(shader, "../../assets/scenes/hw1/bunny.xml")));
 	//init editor
 	Chroma::Editor editor(window, scene.get());
 
