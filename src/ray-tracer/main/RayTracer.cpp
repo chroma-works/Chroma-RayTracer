803dcc153fa889c0fed9c70b2c05df451ac279aa
Alper Ş <alpersahistan@gmail.com>
2020-02-29 03:33:01 +0300

    Ray trace with acceleration structure

--- src/ray-tracer/main/RayTracer.cpp
+++ src/ray-tracer/main/RayTracer.cpp
@@ -92,56 +92,44 @@ namespace Chroma
 				glm::vec3 color = scene.m_sky_color;
 
 				camera_ray.direction = glm::normalize(top_left_w + right_step * (i + 0.5f) + down_step * (j + 0.5f) - camera_ray.origin);
-
 				//Go over scene objects and lights
 				float t_min = std::numeric_limits<float>::max();
-				std::map<std::string, std::shared_ptr<SceneObject>>::iterator it;
-				for (it = scene.m_scene_objects.begin(); it != scene.m_scene_objects.end(); it++)
+				if (scene.m_accel_structure->Intersect(camera_ray, scene.m_intersect_eps, intersection_data))//Hit
 				{
-					if (it->second->IsVisible() && it->second->Intersect(camera_ray, m_settings.intersection_eps, intersection_data)
-						&& (glm::distance(camera_ray.origin, intersection_data->position) < t_min))//Hit
+					color = { 0,0,0 };
+					//lighting calculation
+					std::map<std::string, std::shared_ptr<PointLight>>::iterator it2;
+					for (it2 = scene.m_point_lights.begin(); it2 != scene.m_point_lights.end(); it2++)
 					{
-						color = { 0,0,0 };
-						t_min = glm::distance(camera_ray.origin, intersection_data->position);
-						//lighting calculation
-						std::map<std::string, std::shared_ptr<PointLight>>::iterator it2;
-						for (it2 = scene.m_point_lights.begin(); it2 != scene.m_point_lights.end(); it2++)
+						std::shared_ptr<PointLight> pl = it2->second;
+						glm::vec3 e_vec = glm::normalize(camera_ray.origin - intersection_data->position);
+						glm::vec3 l_vec = glm::normalize(pl->position - intersection_data->position);
+
+						//Shadow calculation	
+						bool shadowed = false;
+						Ray shadow_ray(intersection_data->position + l_vec * scene.m_shadow_eps);
+						shadow_ray.direction = glm::normalize(pl->position - shadow_ray.origin);
+							
+						shadowed = m_settings.calc_shadows && (scene.m_accel_structure->Intersect(shadow_ray, scene.m_intersect_eps, shadow_data) &&
+							shadow_data->t < glm::distance(intersection_data->position, pl->position));
+
+						if (!shadowed)
 						{
-							std::shared_ptr<PointLight> pl = it2->second;
-							glm::vec3 e_vec = glm::normalize(camera_ray.origin - intersection_data->position);
-							glm::vec3 l_vec = glm::normalize(pl->position - intersection_data->position);
+							float d = glm::distance(pl->position, intersection_data->position);
 
-							//Shadow calculation	
-							bool shadowed = false;
-							for (auto it3 = scene.m_scene_objects.begin(); !shadowed &&
-								it3 != scene.m_scene_objects.end() && m_settings.calc_shadows; it3++)
-							{
-								Ray shadow_ray(intersection_data->position + l_vec * scene.m_shadow_eps);
-								shadow_ray.direction = glm::normalize(pl->position - shadow_ray.origin);
-								shadowed = it3->second->IsVisible() && it3->second->Intersect(shadow_ray, m_settings.intersection_eps, shadow_data) &&
-									glm::distance(shadow_data->position, intersection_data->position) < glm::distance(intersection_data->position, pl->position);
-							}
-
-							if (!shadowed)
-							{
-								float d = glm::distance(pl->position, intersection_data->position);
-
-								//Kd * I * cos(theta) /d^2 
-								glm::vec3 diffuse = intersection_data->material->diffuse * pl->intensity *
-									glm::max(glm::dot(intersection_data->normal, l_vec), 0.0f) / (glm::length(intersection_data->normal) * glm::length(l_vec)) / (d * d);
-								//Ks* I * max(0, r . dir) / d^2
-								glm::vec3 h = glm::normalize((e_vec + l_vec) / glm::length(e_vec + l_vec));
-								glm::vec3 specular = intersection_data->material->specular * pl->intensity *
-									glm::pow(glm::max(0.0f, glm::dot(h, glm::normalize(intersection_data->normal))), it->second->GetMaterial()->shininess) / (d * d);
-								//glm::pow(glm::max(0.0f, glm::dot(r, camera_ray.direction)), it->second->GetMaterial()->shininess) / (d*d);
-							//specular = (_isnan(specular.x) || _isnan(specular.y) || _isnan(specular.z)) ? glm::vec3({0, 0, 0}) : specular;
-								color += specular + diffuse;
-							}
+							//Kd * I * cos(theta) /d^2 
+							glm::vec3 diffuse = intersection_data->material->diffuse * pl->intensity *
+								glm::max(glm::dot(intersection_data->normal, l_vec), 0.0f) / (glm::length(intersection_data->normal) * glm::length(l_vec)) / (d * d);
+							//Ks* I * max(0, h . n)^s / d^2
+							glm::vec3 h = glm::normalize((e_vec + l_vec) / glm::length(e_vec + l_vec));
+							glm::vec3 specular = intersection_data->material->specular * pl->intensity *
+								glm::pow(glm::max(0.0f, glm::dot(h, glm::normalize(intersection_data->normal))), intersection_data->material->shininess) / (d * d);
+							color += specular + diffuse;
 						}
-						//Ka * Ia
-						glm::vec3 ambient = scene.m_ambient_l * intersection_data->material->ambient;
-						color += ambient;
 					}
+					//Ka * Ia
+					glm::vec3 ambient = scene.m_ambient_l * intersection_data->material->ambient;
+					color += ambient;
 				}
 				m_rendered_image->SetPixel(i, j, glm::clamp(color, 0.0f, 255.0f));
 			}
