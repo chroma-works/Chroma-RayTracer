0386f4c678c6f52f39bf82b3c265a1bd1087737d
Alper Ş <alpersahistan@gmail.com>
2020-04-08 01:29:31 +0300

    Fix transform matrix calculation order

--- src/ray-tracer/editor/AssetImporter.cpp
+++ src/ray-tracer/editor/AssetImporter.cpp
@@ -128,7 +128,7 @@ namespace Chroma
 		Split(data, v, ' ');
 		glm::mat4 trnsfm = glm::mat4(1.0f);
 
-		for (int i = 0; i < v.size(); i++)
+		for (int i = v.size()-1; i >=0 ; i--)
 		{
 			if (v[i].at(0) == 't')
 			{
