07312818089c53df402b036ba70e8aad0e67db39
Alper Ş <alpersahistan@gmail.com>
2020-04-17 20:55:50 +0300

    Set up a custom scene

--- src/ray-tracer/main/EntryPoint.cpp
+++ src/ray-tracer/main/EntryPoint.cpp
@@ -32,7 +32,7 @@ int main()
 	Chroma::Shader* shader = Chroma::Shader::ReadAndBuildShaderFromFile("../../assets/shaders/phong.vert", "../../assets/shaders/phong.frag");
 
 	std::shared_ptr<Chroma::Scene> scene;
-	scene = std::make_shared<Chroma::Scene>(*(Chroma::AssetImporter::LoadSceneFromXML(shader, "../../assets/scenes/hw2/spheres.xml")));
+	scene = std::make_shared<Chroma::Scene>(*(Chroma::AssetImporter::LoadSceneFromXML(shader, "../../assets/scenes/hw3/spheres_dof.xml")));
 	//init editor
 	Chroma::Editor editor(&window, scene.get());
 
@@ -40,28 +40,50 @@ int main()
 	//editor.TogglePreviewRender(false);
 
 	//Model import
-	/*auto r_mesh = std::make_shared<Chroma::Mesh>(*Chroma::AssetImporter::LoadMeshFromOBJ("../../assets/models/rabbit.obj"));
-	Chroma::Texture* text = new Chroma::Texture("../../assets/textures/white.png");
-	std::shared_ptr<Chroma::Material> mat = std::make_shared<Chroma::Material>("u_Material",
+	auto d_mesh = std::make_shared<Chroma::Mesh>(*Chroma::AssetImporter::LoadMeshFromOBJ("../../assets/models/dragon.obj"));
+	//Chroma::Texture* text = new Chroma::Texture("../../assets/textures/white.png");
+	std::shared_ptr<Chroma::Dielectric> mat = std::make_shared<Chroma::Dielectric>("u_Material",
+		glm::vec3(), glm::vec3(), glm::vec3(), 0.0f);
+	mat->m_refraction_ind = 1.56;
+	mat->m_absorption_coeff = { 0.15f, 0.15f, 0.0f };
+	std::shared_ptr<Chroma::SceneObject> dragon = std::make_shared<Chroma::SceneObject>(d_mesh, "dragon");
+	dragon->SetMaterial(mat);
+
+
+	//Model import
+	auto t_mesh = std::make_shared<Chroma::Mesh>(*Chroma::AssetImporter::LoadMeshFromOBJ("../../assets/models/utah_teapot.obj"));
+	std::shared_ptr<Chroma::Material> mat2 = std::make_shared<Chroma::Material>("u_Material",
 		glm::vec3({ 0.8f, 0.8f, 0.8f }), glm::vec3({ 0.8f, 0.8f, 0.8f }), glm::vec3({ 1.0f, 1.0f, 1.0f }), 90.0f);
+	std::shared_ptr<Chroma::SceneObject> teapot = std::make_shared<Chroma::SceneObject>(t_mesh, "teapot");
+	//teapot->SmoothNormals();
+	teapot->SetMaterial(mat2);
+
+	//Model import
+	auto r_mesh = std::make_shared<Chroma::Mesh>(*Chroma::AssetImporter::LoadMeshFromOBJ("../../assets/models/rabbit.obj"));
+	std::shared_ptr<Chroma::Conductor> mat3 = std::make_shared<Chroma::Conductor>("u_Material",
+		glm::vec3({ 0.0f, 0.0f, 0.0f }), glm::vec3({ 0.0f, 0.0f, 0.0f }), glm::vec3({ 0.71f, 0.651f, 0.26f }) / 8.0f, 1.0f);
+	mat3->m_absorption_ind = 3.1350;
+	mat3->m_roughness = 0.5f;
+	mat3->m_refraction_ind = 0.46192f;
+	mat3->m_mirror_reflec = { 0.71f, 0.651f, 0.26f };
 	std::shared_ptr<Chroma::SceneObject> rabbit = std::make_shared<Chroma::SceneObject>(r_mesh, "rabbit");
-	rabbit->SetMaterial(mat);*/
-	/*rabbit->SetTexture(*texture);
-	teapot->SetTexture(*text);*/
+	rabbit->SetMaterial(mat3);
 
-	////Model import
-	//auto t_mesh = std::make_shared<Chroma::Mesh>(*Chroma::AssetImporter::LoadMeshFromOBJ("../../assets/models/utah_teapot.obj"));
-	////Chroma::Texture* text = new Chroma::Texture("../../assets/textures/white.png");
-	//std::shared_ptr<Chroma::Material> mat2 = std::make_shared<Chroma::Material>("u_Material",
-	//	glm::vec3({ 0.8f, 0.8f, 0.8f }), glm::vec3({ 0.8f, 0.8f, 0.8f }), glm::vec3({ 1.0f, 1.0f, 1.0f }), 90.0f);
-	//std::shared_ptr<Chroma::SceneObject> teapot = std::make_shared<Chroma::SceneObject>(t_mesh, "teapot");
-	////teapot->SmoothNormals();
-	//rabbit->SetMaterial(mat2);
+	//Model import
+	auto o_mesh = std::make_shared<Chroma::Mesh>(*Chroma::AssetImporter::LoadMeshFromOBJ("../../assets/models/torus_knot.obj"));
+	std::shared_ptr<Chroma::Conductor> mat4 = std::make_shared<Chroma::Conductor>("u_Material",
+		glm::vec3({ 0.0f, 0.0f, 0.0f }), glm::vec3({ 0.0f, 0.0f, 0.0f }), glm::vec3({ 0.75f, 0.816f, 0.82f }) / 8.0f, 1.0f);
+	mat4->m_absorption_ind = 4.0504;
+	mat4->m_roughness = 0.05f;
+	mat4->m_refraction_ind = 1.6912;
+	mat4->m_mirror_reflec = { 0.75f, 0.816f, 0.82f };
+	std::shared_ptr<Chroma::SceneObject> torus = std::make_shared<Chroma::SceneObject>(o_mesh, "torus");
+	torus->SetMaterial(mat4);
 
-	 //std::make_shared<Chroma::Scene>("The scene", shader);*/
-	//scene->AddSceneObject(rabbit->GetName(), rabbit);
-	//scene->AddSceneObject(teapot->GetName(), teapot);
-	//scene->AddSceneObject("box", box);
+	scene->AddSceneObject(dragon->GetName(), dragon);
+	scene->AddSceneObject(teapot->GetName(), teapot);
+	scene->AddSceneObject(rabbit->GetName(), rabbit);
+	scene->AddSceneObject(torus->GetName(), torus);
 
 	glEnable(GL_DEPTH_TEST);
 
@@ -86,6 +108,21 @@ int main()
 		//scene->AddLight("spot 1", sl);
 		//scene->AddLight("directional 1", dl);
 
+	dragon->SetScale({ 11, 11, 11 });
+	dragon->SetPosition({ -1.7,-7.150, 1.8 });
+	dragon->SetRotation({0, 125, 0});
+
+	teapot->SetScale({ 2.2,2.2,2.2 });
+	teapot->SetPosition({ 3.550,-10.25, 4.5 });
+	teapot->SetRotation({ 0,56,0 });
+
+	rabbit->SetScale({ 0.170,0.170, 0.170 });
+	rabbit->SetPosition({ -7.250, -10.25, 1.35 });
+	rabbit->SetRotation({ -90, 35,0 });
+
+	torus->SetScale({ 1.3, 1.3, 1.3 });
+	torus->SetPosition({ 6.800, -7.9, 2.5 });
+	torus->SetRotation({ 27, -12,0 });
 	/*glm::vec4 dir({ 0.0f, 0.0f, 0.0f, 1.0f });
 
 	teapot->SetScale({ 10.0f, 10.0f, 10.0f });
