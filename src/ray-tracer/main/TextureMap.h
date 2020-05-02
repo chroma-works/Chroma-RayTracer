e2dd2e0ffcaa869f280038df4ed8e4de39067d95
Alper Ş <alpersahistan@gmail.com>
2020-05-02 17:02:51 +0300

    Implement soruce type checking

--- src/ray-tracer/main/TextureMap.h
+++ src/ray-tracer/main/TextureMap.h
@@ -11,13 +11,29 @@ namespace Chroma
 	{
 	public:
 		virtual glm::vec3 SampleAt(glm::vec3 p) const = 0;
+		virtual glm::vec3 BumpAt(glm::vec3 p) const = 0;
+
+		inline void SetBumpFactor( float bf) 
+		{
+			if (m_decal_mode == DECAL_M::bump)
+				m_bump_factor = bf;
+		}
 
 		inline DECAL_M GetDecalMode() { return m_decal_mode; }
 		inline SOURCE_T GetType() { return m_type; }
+		inline float GetBumpFactor() 
+		{ 
+			if (m_decal_mode == DECAL_M::bump)
+				return m_bump_factor;
+			else
+				return NAN;
+		}
 		//inline void SetDecalMode(DECAL_M mode) { m_decal_mode = mode; }
 		
 	protected:
 		DECAL_M m_decal_mode;
 		SOURCE_T m_type;
+
+		float m_bump_factor = 1.0f; 
 	};
 }
