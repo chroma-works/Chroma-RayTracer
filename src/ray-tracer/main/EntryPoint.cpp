297fee02ae6b11d606b4650e3b7750f159d9bd90
Alper Ş <alpersahistan@gmail.com>
2020-05-20 17:34:24 +0300

    Upload hw5 scenes

--- src/ray-tracer/main/EntryPoint.cpp
+++ src/ray-tracer/main/EntryPoint.cpp
@@ -32,7 +32,7 @@ int main()
 
 	std::shared_ptr<Chroma::Scene> scene;
 	Chroma::Settings s;
-	scene = std::make_shared<Chroma::Scene>(*(Chroma::AssetImporter::LoadSceneFromXML(shader, "../../assets/scenes/hw4/killeroo_bump_walls.xml", &s)));
+	scene = std::make_shared<Chroma::Scene>(*(Chroma::AssetImporter::LoadSceneFromXML(shader, "../../assets/scenes/hw4/sphere_nobump_justbump.xml", &s)));
 	//init editor
 	Chroma::Editor editor(&window, scene.get(), s);
 
