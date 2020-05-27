84f07cffa8f9ccb32e27248ddb6088a8f56a99b5
Alper Ş <alpersahistan@gmail.com>
2020-05-27 21:31:31 +0300

    Make random engine static global

--- src/ray-tracer/main/Utilities.h
+++ src/ray-tracer/main/Utilities.h
@@ -9,32 +9,32 @@
 
 namespace Utils
 {
+	thread_local static std::random_device s_rd;
+	thread_local static std::mt19937_64 s_gen(s_rd());
+
 	inline glm::vec2 SampleUnitSquare()
 	{
-		std::random_device rd;  //Will be used to obtain a seed for the random number engine
-		std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+		/*std::random_device rd;  //Will be used to obtain a seed for the random number engine
+		std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()*/
 		std::uniform_real_distribution<> dis(0.0, 1.0);
-		return { dis(gen), dis(gen) };
+		return { dis(s_gen), dis(s_gen) };
 	}
 	inline glm::vec2 SampleUnitDisk()
 	{
-		std::random_device rd;  //Will be used to obtain a seed for the random number engine
-		std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()
+		/*std::random_device rd;  //Will be used to obtain a seed for the random number engine
+		std::mt19937 gen(rd()); //Standard mersenne_twister_engine seeded with rd()*/
 		std::uniform_real_distribution<double> dis(0.0, 1.0f);
 		const double pi2 = 2.0f * glm::pi<double>();
-		double theta = pi2 * dis(gen);
-		double r = dis(gen);
+		double theta = pi2 * dis(s_gen);
+		double r = dis(s_gen);
 
 		return { r * cos(theta), r * sin(theta) };
 	}
 
 	inline float RandFloat(float l = 0.0f, float u = 1.0f)
 	{
-		std::random_device rd;
-		std::mt19937 gen(rd());
-		std::uniform_real_distribution<float> dis(l, u);
-
-		return dis(gen);
+		thread_local std::uniform_real_distribution<double> dis(l, u);
+		return dis(s_gen);
 	}
 
 	inline glm::vec3 CalculateNonColinearTo(glm::vec3 r)
