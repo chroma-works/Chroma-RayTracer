daf9094f4041771dffc9a0dbcd9b07f61d957a0b
Alper Ş <alpersahistan@gmail.com>
2020-06-23 19:48:46 +0300

    Make settings singleton and subject

--- src/ray-tracer/editor/Settings.h
+++ src/ray-tracer/editor/Settings.h
@@ -1,24 +1,49 @@
 #pragma once
 #include <string>
+#include <list>
 #include <thirdparty/glm/glm/glm.hpp>
+
+#include "Observer.h"
 
 namespace CHR
 {
 	enum IM_POST_PROC_T{none=0, clamp = 1, tone_map=2 };//none means already ldr
-	struct Settings
+	class Settings : public Subject
 	{
-		int thread_count = 8;
-		std::string act_editor_cam_name = "";
-		std::string act_rt_cam_name = "";
-		glm::ivec2 resolution = {800, 800};
-		float camera_move_speed = 0.4;
-		float camera_rotate_speed = 0.005;
-		float shadow_eps = 0.01;
-		float intersection_eps = 0.000001;
-		bool calc_shadows = true;
-		bool calc_reflections = true;
-		bool calc_refractions = true;
-		int recur_depth = 6;
-		IM_POST_PROC_T ldr_post_process = none;
+	public:
+		static Settings* GetInstance() 
+		{
+			if (!s_instance)
+				s_instance = new Settings;
+			return s_instance;
+		}
+
+		void SetResolution(glm::ivec2 res);
+		inline glm::ivec2 GetResolution() const { return m_resolution; }
+
+		void Attach(Observer* observer);
+		void Detach(Observer* observer) ;
+		void Notify();
+
+		int m_thread_count = 8;
+		std::string m_act_editor_cam_name = "";
+		std::string m_act_rt_cam_name = "";
+		float m_camera_move_speed = 0.4;
+		float m_camera_rotate_speed = 0.005;
+		float m_shadow_eps = 0.01;
+		float m_intersection_eps = 0.000001;
+		bool m_calc_shadows = true;
+		bool m_calc_reflections = true;
+		bool m_calc_refractions = true;
+		int m_recur_depth = 6;
+		IM_POST_PROC_T m_ldr_post_process = none;
+	private:
+		Settings(){}
+		static Settings* s_instance;
+
+		std::list<Observer*> m_observers;
+
+		glm::ivec2 m_resolution = {800, 800};
+
 	};
 }
