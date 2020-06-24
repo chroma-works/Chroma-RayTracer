6382b8bf96bb0e2ece69a07f86bc6cfe9b655496
Alper Ş <alpersahistan@gmail.com>
2020-06-24 05:04:25 +0300

    Fix direction calculation

--- src/ray-tracer/main/ObjectLight.h
+++ src/ray-tracer/main/ObjectLight.h
@@ -19,20 +19,20 @@ namespace CHR
 		{
 			//Transform intersenction position to object space
 			glm::vec3 obj_isect_pos = *m_inv_transform * glm::vec4(isect_pos, 1.0f);
-			float cos_t_max = sqrt(1 - pow(1/glm::length(obj_isect_pos), 2.0f));
+			float cos_t_max = sqrt(1.0f - pow(1/glm::length(obj_isect_pos), 2.0f));
 
 			float chi_1 = CHR_UTILS::RandFloat(), chi_2 = CHR_UTILS::RandFloat();
 			float theta = acosf(1.0f - chi_1 + chi_1 * cos_t_max), phi = 2 * CHR_UTILS::PI * chi_2;
 
 			glm::vec3 w = glm::normalize(glm::vec3(0, 0, 0) - obj_isect_pos), u, v;
 			CHR_UTILS::GenerateONB(w, u, v);
-			glm::vec3 l_local = w* cos(theta) + v * sin(theta) * cos(phi) + u * sin(theta) * cos(phi);
+			glm::vec3 l_local = w* cos(theta) + v * sin(theta) * cos(phi) + u * sin(theta) * sin(phi);
 
-			/*Ray ray(isect_pos, glm::normalize(*m_transform * glm::vec4(l_local, 0.0f)));//p_world, l_world
-			IntersectionData isect_data;
-			
-			Intersect(ray, &isect_data);*/
-			return glm::normalize(*m_inv_transform * glm::vec4(l_local,0));
+			Ray ray(isect_pos, glm::normalize(*m_transform * glm::vec4(l_local, 0.0f)));//p_world, l_world
+			IntersectionData light_isect;
+			Sphere::Intersect(ray, &light_isect);
+
+			return glm::normalize(light_isect.position - isect_pos);
 		}
 
 		glm::vec3 RadianceAt(const glm::vec3 isect_pos, const glm::vec3 l_vec) const
